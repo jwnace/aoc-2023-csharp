a1c8a0cff070e426009f54eeb8a3271c840815a8
Joe Nace <jwnace@gmail.com>
2023-10-27 14:28:38 -0400

    disabled the `ConsoleSpinner` for now

--- src/aoc-2023-csharp/Common/Runner.cs
+++ src/aoc-2023-csharp/Common/Runner.cs
@@ -7,11 +7,11 @@ internal static class Runner
     public static async Task RunPart<TResult>(int day, int part, Func<TResult> callback)
     {
         var stopwatch = Stopwatch.StartNew();
-        var spinner = StartSpinner(day, part, stopwatch);
+        // var spinner = StartSpinner(day, part, stopwatch);
         var result = await Task.Run(callback);
 
         stopwatch.Stop();
-        spinner.Stop();
+        // spinner.Stop();
 
         Console.WriteLine($"Day {day.ToString(),2}, Part {part.ToString(),2} {stopwatch.Elapsed} => {result}");
     }
