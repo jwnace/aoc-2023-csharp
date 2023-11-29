90a7e28eff28a749c2a6cdd556aa3c064da28276
Joe Nace <jwnace@gmail.com>
2023-11-29 14:13:54 -0500

    added `Deconstruct()` helper methods for Arrays

--- src/aoc-2023-csharp/Extensions/EnumerableExtensions.cs
+++ src/aoc-2023-csharp/Extensions/EnumerableExtensions.cs
@@ -33,4 +33,34 @@ public static class EnumerableExtensions
         return GetPermutations(enumerable, length - 1)
             .SelectMany(x => enumerable.Where(y => !x.Contains(y)), (a, b) => a.Concat(new[] { b }));
     }
+
+    public static void Deconstruct<T>(this T[] array, out T first, out T second)
+    {
+        first = array[0];
+        second = array[1];
+    }
+
+    public static void Deconstruct<T>(this T[] array, out T first, out T second, out T third)
+    {
+        first = array[0];
+        second = array[1];
+        third = array[2];
+    }
+
+    public static void Deconstruct<T>(this T[] array, out T first, out T second, out T third, out T fourth)
+    {
+        first = array[0];
+        second = array[1];
+        third = array[2];
+        fourth = array[3];
+    }
+
+    public static void Deconstruct<T>(this T[] array, out T first, out T second, out T third, out T fourth, out T fifth)
+    {
+        first = array[0];
+        second = array[1];
+        third = array[2];
+        fourth = array[3];
+        fifth = array[4];
+    }
 }
