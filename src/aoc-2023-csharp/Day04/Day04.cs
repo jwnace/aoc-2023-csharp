d22668e50f248ae6455cf9cafafd3968aa260d88
Joe Nace <jwnace@gmail.com>
2023-12-04 00:26:18 -0500

    initial solution for day 4
    initial solution for day 4

--- src/aoc-2023-csharp/Day04/Day04.cs
+++ src/aoc-2023-csharp/Day04/Day04.cs
@@ -1,20 +1,87 @@
-﻿namespace aoc_2023_csharp.Day04;
+﻿using aoc_2023_csharp.Extensions;
+
+namespace aoc_2023_csharp.Day04;
 
 public static class Day04
 {
-    private static readonly string Input = File.ReadAllText("Day04/day04.txt").Trim();
+    private static readonly string[] Input = File.ReadAllLines("Day04/day04.txt").ToArray();
 
     public static int Part1() => Solve1(Input);
 
     public static int Part2() => Solve2(Input);
 
-    public static int Solve1(string input)
+    public static int Solve1(string[] input)
     {
-        return 0;
+        var total = 0;
+
+        foreach (var line in input)
+        {
+            var (left, right) = line.Split(": ");
+
+            var (l, r) = right.Split(" | ");
+
+            var winningNumbers = l.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var numbersYouHave = r.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var intersection = winningNumbers.Intersect(numbersYouHave);
+            var count = intersection.Count();
+
+            var score = Convert.ToInt32(Math.Pow(2, count - 1));
+
+            total += score;
+        }
+
+        return total;
     }
 
-    public static int Solve2(string input)
+    public static int Solve2(string[] input)
     {
-        return 0;
+        var scratchCards = GetScratchCards(input).ToList();
+
+        var min = scratchCards.Min(x => x.CardNumber);
+        var max = scratchCards.Max(x => x.CardNumber);
+
+        for (var i = min; i <= max; i++)
+        {
+            var cardCount = scratchCards.Count(x => x.CardNumber == i);
+
+            if (cardCount > 0)
+            {
+                var card = scratchCards.First(x => x.CardNumber == i);
+                var winCount = card.WinningNumbers.Intersect(card.NumbersYouHave).Count();
+
+                if (winCount > 0)
+                {
+                    for (var k = 0; k < cardCount; k++)
+                    {
+                        for (var j = 1; j <= winCount; j++)
+                        {
+                            var cardToCopy = scratchCards.First(x => x.CardNumber == i + j);
+                            scratchCards.Add(cardToCopy);
+                        }
+                    }
+                }
+            }
+        }
+
+        return scratchCards.Count;
     }
+
+    private static IEnumerable<ScratchCard> GetScratchCards(string[] input)
+    {
+        foreach (var line in input)
+        {
+            var (left, right) = line.Split(": ");
+            var cardNumber = int.Parse(left[5..]);
+
+            var (winningNumbers, numbersYouHave) = right.Split(" | ");
+
+            var winningNumbersArray = winningNumbers.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var numbersYouHaveArray = numbersYouHave.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            yield return new ScratchCard(cardNumber, winningNumbersArray, numbersYouHaveArray);
+        }
+    }
+
+    private record ScratchCard(int CardNumber, int[] WinningNumbers, int[] NumbersYouHave);
 }
--- tests/aoc-2023-csharp-tests/Day04Tests.cs
+++ tests/aoc-2023-csharp-tests/Day04Tests.cs
@@ -8,8 +8,17 @@ public class Day04Tests
     public void Part1_Example()
     {
         // arrange
-        var input = "";
-        var expected = 0;
+        var input = new[]
+        {
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        };
+
+        var expected = 13;
 
         // act
         var actual = Day04.Solve1(input);
@@ -21,15 +30,24 @@ public class Day04Tests
     [Test]
     public void Part1_Solution()
     {
-        Day04.Part1().Should().Be(0);
+        Day04.Part1().Should().Be(21158);
     }
 
     [Test]
     public void Part2_Example()
     {
         // arrange
-        var input = "";
-        var expected = 0;
+        var input = new[]
+        {
+            "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+            "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+            "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+            "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+            "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+            "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        };
+
+        var expected = 30;
 
         // act
         var actual = Day04.Solve2(input);
@@ -41,6 +59,6 @@ public class Day04Tests
     [Test]
     public void Part2_Solution()
     {
-        Day04.Part2().Should().Be(0);
+        Day04.Part2().Should().Be(6050769);
     }
 }
