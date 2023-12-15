019899467a6ff5ce83e63595d052986833353871
Joe Nace <jwnace@gmail.com>
2023-12-15 00:06:04 -0500

    initial solution for part 1

--- src/aoc-2023-csharp/Day15/Day15.cs
+++ src/aoc-2023-csharp/Day15/Day15.cs
@@ -8,13 +8,25 @@ public static class Day15
 
     public static int Part2() => Solve2(Input);
 
-    public static int Solve1(string input)
-    {
-        return 0;
-    }
+    public static int Solve1(string input) => input.Split(",").Select(ComputeHash).Sum();
 
     public static int Solve2(string input)
     {
         return 0;
+    }
+
+    private static int ComputeHash(string input)
+    {
+        var result = 0;
+
+        foreach (var c in input)
+        {
+            var ascii = (int)c;
+            result += ascii;
+            result *= 17;
+            result %= 256;
+        }
+
+        return result;
     }
 }
