8c33c36bc07153cac438b79c8878a4b13877a770
Joe Nace <jwnace@gmail.com>
2023-12-17 02:08:35 -0500

    refactoring and cleanup

--- src/aoc-2023-csharp/Day17/Day17.cs
+++ src/aoc-2023-csharp/Day17/Day17.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-
-namespace aoc_2023_csharp.Day17;
+﻿namespace aoc_2023_csharp.Day17;
 
 public static class Day17
 {
@@ -12,7 +8,11 @@ public static class Day17
 
     public static int Part2() => Solve2(Input);
 
-    public static int Solve1(string[] input)
+    public static int Solve1(string[] input) => Solve(input, 1);
+
+    public static int Solve2(string[] input) => Solve(input, 2);
+
+    private static int Solve(string[] input, int part)
     {
         var grid = BuildGrid(input);
 
@@ -21,42 +21,35 @@ public static class Day17
         var minCol = grid.Keys.Min(x => x.col);
         var maxCol = grid.Keys.Max(x => x.col);
 
-        var initialState = (row: 0, col: 0, direction: Direction.None, steps: 0, heatLoss: 0);
-
-        var queue = new PriorityQueue<(int row, int col, Direction direction, int steps, int heatLoss), int>();
-        queue.Enqueue(initialState, 0);
-
-        var seen = new HashSet<(int row, int col, Direction direction, int steps)>();
-
-        var destination = (maxRow, maxCol);
+        var seen = new HashSet<StateKey>();
+        var queue = new PriorityQueue<State, int>();
+        queue.Enqueue(new State(0, 0, Direction.None, 0, 0), 0);
 
         while (queue.Count > 0)
         {
-            var state = queue.Dequeue();
-            var (row, col, direction, steps, heatLoss) = state;
-            var key = (row, col, direction, steps);
+            var (row, col, direction, steps, heatLoss) = queue.Dequeue();
+            var key = new StateKey(row, col, direction, steps);
 
-            if (seen.Contains(key))
+            if (!seen.Add(key))
             {
                 continue;
             }
 
-            seen.Add(key);
-
-            // we've reached the end
-            if ((row, col) == destination)
+            if (part == 1 && (row, col) == (maxRow, maxCol))
             {
                 return heatLoss;
             }
 
-            // we went off the edge
-            if (row < minRow || row > maxRow || col < minCol || col > maxCol)
+            if (part == 2 && (row, col) == (maxRow, maxCol) && steps > 3)
             {
-                continue;
+                return heatLoss;
             }
 
-            // add all adjacent tiles to the queue
-            foreach (var (nextRow, nextCol, nextDirection) in GetAdjacentTiles(row, col, steps, direction))
+            var neighbors = part == 1
+                ? GetNeighbors(row, col, steps, direction, minSteps: 0, maxSteps: 3)
+                : GetNeighbors(row, col, steps, direction, minSteps: 4, maxSteps: 10);
+
+            foreach (var (nextRow, nextCol, nextDirection) in neighbors)
             {
                 if (nextRow < minRow || nextRow > maxRow || nextCol < minCol || nextCol > maxCol)
                 {
@@ -69,205 +62,11 @@ public static class Day17
                 }
 
                 var nextHeatLoss = heatLoss + grid[(nextRow, nextCol)];
-                queue.Enqueue((nextRow, nextCol, nextDirection, steps + 1, nextHeatLoss), nextHeatLoss);
+                queue.Enqueue(new State(nextRow, nextCol, nextDirection, steps + 1, nextHeatLoss), nextHeatLoss);
             }
         }
 
         throw new Exception("No path found");
-    }
-
-    public static int Solve2(string[] input)
-    {
-        var grid = BuildGrid(input);
-
-        var minRow = grid.Keys.Min(x => x.row);
-        var maxRow = grid.Keys.Max(x => x.row);
-        var minCol = grid.Keys.Min(x => x.col);
-        var maxCol = grid.Keys.Max(x => x.col);
-
-        var initialState = (row: 0, col: 0, direction: Direction.None, steps: 0, heatLoss: 0);
-
-        var queue = new PriorityQueue<(int row, int col, Direction direction, int steps, int heatLoss), int>();
-        queue.Enqueue(initialState, 0);
-
-        var seen = new HashSet<(int row, int col, Direction direction, int steps)>();
-
-        var destination = (maxRow, maxCol);
-
-        while (queue.Count > 0)
-        {
-            var state = queue.Dequeue();
-            var (row, col, direction, steps, heatLoss) = state;
-            var key = (row, col, direction, steps);
-
-            if (seen.Contains(key))
-            {
-                continue;
-            }
-
-            seen.Add(key);
-
-            // we've reached the end
-            if ((row, col) == destination && steps > 3)
-            {
-                return heatLoss;
-            }
-
-            // we went off the edge
-            if (row < minRow || row > maxRow || col < minCol || col > maxCol)
-            {
-                continue;
-            }
-
-            // add all adjacent tiles to the queue
-            foreach (var (nextRow, nextCol, nextDirection) in GetAdjacentTiles2(row, col, steps, direction))
-            {
-                if (nextRow < minRow || nextRow > maxRow || nextCol < minCol || nextCol > maxCol)
-                {
-                    continue;
-                }
-
-                if (direction != nextDirection)
-                {
-                    steps = 0;
-                }
-
-                var nextHeatLoss = heatLoss + grid[(nextRow, nextCol)];
-                queue.Enqueue((nextRow, nextCol, nextDirection, steps + 1, nextHeatLoss), nextHeatLoss);
-            }
-        }
-
-        throw new Exception("No path found");
-    }
-
-    private static IEnumerable<(int, int, Direction)> GetAdjacentTiles(
-        int row,
-        int col,
-        int steps,
-        Direction direction)
-    {
-        if (direction == Direction.None)
-        {
-            yield return (row, col + 1, Direction.Right);
-            yield return (row + 1, col, Direction.Down);
-        }
-        else if (direction == Direction.Right)
-        {
-            if (steps < 3)
-            {
-                yield return (row, col + 1, Direction.Right);
-            }
-
-            yield return (row - 1, col, Direction.Up);
-            yield return (row + 1, col, Direction.Down);
-        }
-        else if (direction == Direction.Left)
-        {
-            if (steps < 3)
-            {
-                yield return (row, col - 1, Direction.Left);
-            }
-
-            yield return (row - 1, col, Direction.Up);
-            yield return (row + 1, col, Direction.Down);
-        }
-        else if (direction == Direction.Up)
-        {
-            if (steps < 3)
-            {
-                yield return (row - 1, col, Direction.Up);
-            }
-
-            yield return (row, col - 1, Direction.Left);
-            yield return (row, col + 1, Direction.Right);
-        }
-        else if (direction == Direction.Down)
-        {
-            if (steps < 3)
-            {
-                yield return (row + 1, col, Direction.Down);
-            }
-
-            yield return (row, col - 1, Direction.Left);
-            yield return (row, col + 1, Direction.Right);
-        }
-    }
-
-    private static IEnumerable<(int, int, Direction)> GetAdjacentTiles2(
-        int row,
-        int col,
-        int steps,
-        Direction direction)
-    {
-        if (direction == Direction.None)
-        {
-            yield return (row, col + 1, Direction.Right);
-            yield return (row + 1, col, Direction.Down);
-        }
-        else if (direction == Direction.Right)
-        {
-            if (steps < 4)
-            {
-                yield return (row, col + 1, Direction.Right);
-                yield break;
-            }
-
-            if (steps < 10)
-            {
-                yield return (row, col + 1, Direction.Right);
-            }
-
-            yield return (row - 1, col, Direction.Up);
-            yield return (row + 1, col, Direction.Down);
-        }
-        else if (direction == Direction.Left)
-        {
-            if (steps < 4)
-            {
-                yield return (row, col - 1, Direction.Left);
-                yield break;
-            }
-
-            if (steps < 10)
-            {
-                yield return (row, col - 1, Direction.Left);
-            }
-
-            yield return (row - 1, col, Direction.Up);
-            yield return (row + 1, col, Direction.Down);
-        }
-        else if (direction == Direction.Up)
-        {
-            if (steps < 4)
-            {
-                yield return (row - 1, col, Direction.Up);
-                yield break;
-            }
-
-            if (steps < 10)
-            {
-                yield return (row - 1, col, Direction.Up);
-            }
-
-            yield return (row, col - 1, Direction.Left);
-            yield return (row, col + 1, Direction.Right);
-        }
-        else if (direction == Direction.Down)
-        {
-            if (steps < 4)
-            {
-                yield return (row + 1, col, Direction.Down);
-                yield break;
-            }
-
-            if (steps < 10)
-            {
-                yield return (row + 1, col, Direction.Down);
-            }
-
-            yield return (row, col - 1, Direction.Left);
-            yield return (row, col + 1, Direction.Right);
-        }
     }
 
     private static Dictionary<(int row, int col), int> BuildGrid(string[] input)
@@ -285,24 +84,88 @@ public static class Day17
         return grid;
     }
 
-    private static string DrawGrid(Dictionary<(int row, int col), int> grid)
+    private static IEnumerable<(int row, int col, Direction direction)> GetNeighbors(
+        int row,
+        int col,
+        int steps,
+        Direction direction,
+        int minSteps = 0,
+        int maxSteps = int.MaxValue)
     {
-        var sb = new StringBuilder();
-        var rows = grid.Keys.Select(k => k.row).Distinct().OrderBy(r => r).ToList();
-        var cols = grid.Keys.Select(k => k.col).Distinct().OrderBy(c => c).ToList();
-
-        foreach (var row in rows)
+        if (direction == Direction.None)
         {
-            foreach (var col in cols)
+            yield return (row, col + 1, Direction.Right);
+            yield return (row + 1, col, Direction.Down);
+        }
+        else if (direction == Direction.Right)
+        {
+            if (steps < minSteps)
             {
-                sb.Append(grid[(row, col)]);
+                yield return (row, col + 1, Direction.Right);
+                yield break;
             }
 
-            sb.AppendLine();
-        }
+            if (steps < maxSteps)
+            {
+                yield return (row, col + 1, Direction.Right);
+            }
 
-        return sb.ToString();
+            yield return (row - 1, col, Direction.Up);
+            yield return (row + 1, col, Direction.Down);
+        }
+        else if (direction == Direction.Left)
+        {
+            if (steps < minSteps)
+            {
+                yield return (row, col - 1, Direction.Left);
+                yield break;
+            }
+
+            if (steps < maxSteps)
+            {
+                yield return (row, col - 1, Direction.Left);
+            }
+
+            yield return (row - 1, col, Direction.Up);
+            yield return (row + 1, col, Direction.Down);
+        }
+        else if (direction == Direction.Up)
+        {
+            if (steps < minSteps)
+            {
+                yield return (row - 1, col, Direction.Up);
+                yield break;
+            }
+
+            if (steps < maxSteps)
+            {
+                yield return (row - 1, col, Direction.Up);
+            }
+
+            yield return (row, col - 1, Direction.Left);
+            yield return (row, col + 1, Direction.Right);
+        }
+        else if (direction == Direction.Down)
+        {
+            if (steps < minSteps)
+            {
+                yield return (row + 1, col, Direction.Down);
+                yield break;
+            }
+
+            if (steps < maxSteps)
+            {
+                yield return (row + 1, col, Direction.Down);
+            }
+
+            yield return (row, col - 1, Direction.Left);
+            yield return (row, col + 1, Direction.Right);
+        }
     }
+
+    private record State(int Row, int Col, Direction Direction, int Steps, int HeatLoss);
+
+    private record StateKey(int Row, int Col, Direction Direction, int Steps);
 
     private enum Direction
     {
