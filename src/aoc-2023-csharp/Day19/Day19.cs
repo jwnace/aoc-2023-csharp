b57d7b35796d8260e908830cb666fbb681317ac5
Joe Nace <jwnace@gmail.com>
2023-12-19 00:58:48 -0500

    initial solution for part 1

--- src/aoc-2023-csharp/Day19/Day19.cs
+++ src/aoc-2023-csharp/Day19/Day19.cs
@@ -1,4 +1,6 @@
-﻿namespace aoc_2023_csharp.Day19;
+﻿using aoc_2023_csharp.Extensions;
+
+namespace aoc_2023_csharp.Day19;
 
 public static class Day19
 {
@@ -6,15 +8,141 @@ public static class Day19
 
     public static int Part1() => Solve1(Input);
 
-    public static int Part2() => Solve2(Input);
+    public static long Part2() => Solve2(Input);
 
     public static int Solve1(string input)
     {
+        var sectionStrings = input.Split("\n\n");
+        var (workflowStrings, partStrings) = sectionStrings.Select(x => x.Split("\n")).ToArray();
+
+        var workflows = workflowStrings.Select(Workflow.Parse).ToDictionary(w => w.Name, w => w);
+        var parts = partStrings.Select(Part.Parse);
+
+        var start = workflows["in"];
+
+        var acceptedParts = parts.Where(part => IsAccepted(part, start, workflows)).ToList();
+
+        return acceptedParts.Sum(x => x.X + x.M + x.A + x.S);
+    }
+
+    public static long Solve2(string input)
+    {
+        var sectionStrings = input.Split("\n\n");
+        var (workflowStrings, _) = sectionStrings.Select(x => x.Split("\n")).ToArray();
+        var workflows = workflowStrings.Select(Workflow.Parse).ToDictionary(w => w.Name, w => w);
+
         return 0;
     }
 
-    public static int Solve2(string input)
+    private static bool IsAccepted(Part part, Workflow workflow, Dictionary<string, Workflow> workflows)
     {
-        return 0;
+        foreach (var rule in workflow.Rules)
+        {
+            if (rule.Type == RuleType.None)
+            {
+                if (rule.NextWorkflowName == "A")
+                {
+                    return true;
+                }
+
+                if (rule.NextWorkflowName == "R")
+                {
+                    return false;
+                }
+
+                return IsAccepted(part, workflows[rule.NextWorkflowName], workflows);
+            }
+
+            var value = rule.Category switch
+            {
+                'x' => part.X,
+                'm' => part.M,
+                'a' => part.A,
+                's' => part.S,
+                _ => throw new Exception("Invalid category")
+            };
+
+            var satisfiesCondition = rule.Type switch
+            {
+                RuleType.GreaterThan => value > rule.Threshold,
+                RuleType.LessThan => value < rule.Threshold,
+                _ => throw new Exception("Invalid rule type")
+            };
+
+            if (satisfiesCondition)
+            {
+                if (rule.NextWorkflowName == "A")
+                {
+                    return true;
+                }
+
+                if (rule.NextWorkflowName == "R")
+                {
+                    return false;
+                }
+
+                return IsAccepted(part, workflows[rule.NextWorkflowName], workflows);
+            }
+        }
+
+        return false;
+    }
+
+    private record Workflow(string Name, Rule[] Rules)
+    {
+        public static Workflow Parse(string text)
+        {
+            var split = text.Split('{');
+            var name = split[0];
+            var ruleStrings = split[1][..^1].Split(',');
+            var rules = ruleStrings.Select(Rule.Parse).ToArray();
+
+            return new Workflow(name, rules);
+        }
+    }
+
+    private record Rule(char Category, RuleType Type, int Threshold, string NextWorkflowName)
+    {
+        public static Rule Parse(string text)
+        {
+            var split = text.Split(':');
+
+            if (split.Length == 1)
+            {
+                return new Rule(' ', RuleType.None, 0, text);
+            }
+
+            var rule = split[0];
+            var nextWorkflowName = split[1];
+
+            var category = rule[0];
+            var comparison = rule[1];
+            var threshold = int.Parse(rule[2..]);
+
+            var type = comparison switch
+            {
+                '<' => RuleType.LessThan,
+                '>' => RuleType.GreaterThan,
+                _ => throw new Exception("Invalid comparison")
+            };
+
+            return new Rule(category, type, threshold, nextWorkflowName);
+        }
+    }
+
+    private enum RuleType
+    {
+        None,
+        GreaterThan,
+        LessThan,
+    }
+
+    private record Part(int X, int M, int A, int S)
+    {
+        public static Part Parse(string arg)
+        {
+            var (x, m, a, s) = arg[1..^1].Split(',').Select(x => int.Parse(x[2..])).ToArray();
+            return new Part(x, m, a, s);
+        }
     }
 }
--- tests/aoc-2023-csharp-tests/Day19Tests.cs
+++ tests/aoc-2023-csharp-tests/Day19Tests.cs
@@ -8,8 +8,28 @@ public class Day19Tests
     public void Part1_Example()
     {
         // arrange
-        var input = "";
-        var expected = 0;
+        var input =
+            """
+            px{a<2006:qkq,m>2090:A,rfg}
+            pv{a>1716:R,A}
+            lnx{m>1548:A,A}
+            rfg{s<537:gd,x>2440:R,A}
+            qs{s>3448:A,lnx}
+            qkq{x<1416:A,crn}
+            crn{x>2662:A,R}
+            in{s<1351:px,qqz}
+            qqz{s>2770:qs,m<1801:hdj,R}
+            gd{a>3333:R,R}
+            hdj{m>838:A,pv}
+
+            {x=787,m=2655,a=1222,s=2876}
+            {x=1679,m=44,a=2067,s=496}
+            {x=2036,m=264,a=79,s=2244}
+            {x=2461,m=1339,a=466,s=291}
+            {x=2127,m=1623,a=2188,s=1013}
+            """;
+
+        var expected = 19114;
 
         // act
         var actual = Day19.Solve1(input);
@@ -21,15 +41,35 @@ public class Day19Tests
     [Test]
     public void Part1_Solution()
     {
-        Day19.Part1().Should().Be(0);
+        Day19.Part1().Should().Be(280909);
     }
 
     [Test]
     public void Part2_Example()
     {
         // arrange
-        var input = "";
-        var expected = 0;
+        var input =
+            """
+            px{a<2006:qkq,m>2090:A,rfg}
+            pv{a>1716:R,A}
+            lnx{m>1548:A,A}
+            rfg{s<537:gd,x>2440:R,A}
+            qs{s>3448:A,lnx}
+            qkq{x<1416:A,crn}
+            crn{x>2662:A,R}
+            in{s<1351:px,qqz}
+            qqz{s>2770:qs,m<1801:hdj,R}
+            gd{a>3333:R,R}
+            hdj{m>838:A,pv}
+
+            {x=787,m=2655,a=1222,s=2876}
+            {x=1679,m=44,a=2067,s=496}
+            {x=2036,m=264,a=79,s=2244}
+            {x=2461,m=1339,a=466,s=291}
+            {x=2127,m=1623,a=2188,s=1013}
+            """;
+
+        var expected = 167409079868000;
 
         // act
         var actual = Day19.Solve2(input);
