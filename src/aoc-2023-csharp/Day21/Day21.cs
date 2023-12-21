3d633c89d4b43ce2df4c93d03005dcf6201afe56
Joe Nace <jwnace@gmail.com>
2023-12-21 17:23:22 -0500

    a bit of refactoring and cleanup

--- src/aoc-2023-csharp/Day21/Day21.cs
+++ src/aoc-2023-csharp/Day21/Day21.cs
@@ -8,88 +8,24 @@ public static class Day21
 
     public static long Part2() => Solve2(Input, 26501365);
 
-    public static int Solve1(string[] input, int goal)
-    {
-        // The Elf starts at the starting position (S) which also counts as a garden plot.
-        var (grid, start) = ParseInput(input);
-
-        // Console.WriteLine(DrawGrid(grid, start));
-
-        var queue = new Queue<(int row, int col, int steps)>();
-        queue.Enqueue((start.row, start.col, 0));
-
-        var seen = new HashSet<(int row, int col, int steps)>();
-
-        var destinations = new HashSet<(int row, int col)>();
-
-        while (queue.Any())
-        {
-            // Console.WriteLine($"Queue: {queue.Count}, Seen: {seen.Count}, Destinations: {destinations.Count}");
-
-            var current = queue.Dequeue();
-
-            if (seen.Contains(current))
-            {
-                continue;
-            }
-
-            seen.Add(current);
-
-            var (row, col, steps) = current;
-            var position = (row, col);
-
-            if (current.steps > goal)
-            {
-                throw new Exception($"Too many steps ({current.steps})");
-            }
-
-            if (current.steps == goal)
-            {
-                // if (destinations.Contains((row, col)))
-                // {
-                //     throw new Exception($"Already visited ({row},{col})");
-                // }
-
-                // TODO: is there a smarter way to do this instead of using a HashSet for deduplication?
-                destinations.Add((row, col));
-                continue;
-            }
-
-            var neighbors = new (int row, int col)[]
-            {
-                (row - 1, col),
-                (row + 1, col),
-                (row, col - 1),
-                (row, col + 1),
-            };
-
-            foreach (var neighbor in neighbors)
-            {
-                if (grid.Contains(neighbor))
-                {
-                    queue.Enqueue((neighbor.row, neighbor.col, steps + 1));
-                }
-            }
-        }
-
-        return destinations.Count;
-    }
+    public static int Solve1(string[] input, int goal) => CountPossibilities(input, goal);
 
     public static long Solve2(string[] input, int goal)
     {
-        long a = GetTerm(input, 65);
-        long b = GetTerm(input, 65 + 131);
-        long c = GetTerm(input, 65 + 131 + 131);
+        // TODO: get rid of these magic numbers
+        var term1 = CountPossibilities(input, 65);
+        var term2 = CountPossibilities(input, 65 + 131);
+        var term3 = CountPossibilities(input, 65 + 131 + 131);
 
-        return DoMath(goal, input.Length, a, b, c);
+        return SolveQuadratic(goal, input.Length, term1, term2, term3);
     }
 
-    private static (HashSet<(int row, int col)> grid, (int row, int col) start) ParseInput(string[] input)
+    private static (HashSet<(int row, int col)> grid, (int row, int col) start) ParseInput(IReadOnlyList<string> input)
     {
         var grid = new HashSet<(int row, int col)>();
         var start = (0, 0);
 
-        for (var row = 0; row < input.Length; row++)
+        for (var row = 0; row < input.Count; row++)
         {
             for (var col = 0; col < input[row].Length; col++)
             {
@@ -108,47 +44,31 @@ public static class Day21
         return (grid, start);
     }
 
-    private static int GetTerm(string[] input, int goal)
+    private static int CountPossibilities(string[] input, int goal)
     {
-        // TODO: figure out if this is generic enough to be used for both part 1 and part 2
+        var (grid, start) = ParseInput(input);
+
         var maxRow = input.Length;
         var maxCol = input[0].Length;
 
-        var (grid, start) = ParseInput(input);
-
+        var destinations = new HashSet<(int row, int col)>();
+        var seen = new HashSet<(int row, int col, int steps)>();
         var queue = new Queue<(int row, int col, int steps)>();
         queue.Enqueue((start.row, start.col, 0));
-
-        var seen = new HashSet<(int row, int col, int steps)>();
-
-        var destinations = new HashSet<(int row, int col)>();
 
         while (queue.Any())
         {
             var current = queue.Dequeue();
 
-            if (seen.Contains(current))
+            if (!seen.Add(current))
             {
                 continue;
             }
 
-            seen.Add(current);
-
             var (row, col, steps) = current;
-            var position = (row, col);
-
-            if (current.steps > goal)
-            {
-                throw new Exception($"Too many steps ({current.steps})");
-            }
 
             if (current.steps == goal)
             {
-                // if (destinations.Contains((row, col)))
-                // {
-                //     throw new Exception($"Already visited ({row},{col})");
-                // }
-
                 destinations.Add((row, col));
                 continue;
             }
@@ -175,18 +95,9 @@ public static class Day21
         return destinations.Count;
     }
 
-    private static long DoMath(long goal, long gridSize, long a, long b, long c)
+    private static (int row, int col) CompressCoordinate((int row, int col) position, int maxRow, int maxCol)
     {
-        var n = goal / gridSize;
-        var b0 = a;
-        var b1 = b - a;
-        var b2 = c - b;
-        return b0 + b1 * n + (n * (n - 1L) / 2L) * (b2 - b1);
-    }
-
-    private static (int row, int col) CompressCoordinate((int row, int col) neighbor, int maxRow, int maxCol)
-    {
-        var (row, col) = neighbor;
+        var (row, col) = position;
 
         while (row < 0)
         {
@@ -199,5 +110,16 @@ public static class Day21
         }
 
         return (row % maxRow, col % maxCol);
+    }
+
+    private static long SolveQuadratic(long goal, long gridSize, long term1, long term2, long term3)
+    {
+        var x = goal / gridSize;
+        var a = (term3 - 2 * term2 + term1) / 2;
+        var b = term2 - term1 - a;
+        var c = term1;
+
+        // a*x^2 + b*x + c
+        return a * x * x + b * x + c;
     }
 }
