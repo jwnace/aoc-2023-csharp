48374eea1a3fa3e7253fb4690be40b611d68bb28
Joe Nace <jwnace@gmail.com>
2023-12-21 05:37:22 -0500

    initial solution for part 1

--- src/aoc-2023-csharp/Day21/Day21.cs
+++ src/aoc-2023-csharp/Day21/Day21.cs
@@ -1,19 +1,135 @@
-﻿namespace aoc_2023_csharp.Day21;
+﻿using System.Text;
+
+namespace aoc_2023_csharp.Day21;
 
 public static class Day21
 {
-    private static readonly string Input = File.ReadAllText("Day21/day21.txt").Trim();
+    private static readonly string[] Input = File.ReadAllLines("Day21/day21.txt").ToArray();
 
-    public static int Part1() => Solve1(Input);
+    public static int Part1() => Solve1(Input, 64);
 
     public static int Part2() => Solve2(Input);
 
-    public static int Solve1(string input)
+    public static int Solve1(string[] input, int goal)
     {
-        return 0;
+        // The Elf starts at the starting position (S) which also counts as a garden plot.
+        var (grid, start) = ParseInput(input);
+
+        // Console.WriteLine(DrawGrid(grid, start));
+
+        var queue = new Queue<(int row, int col, int steps)>();
+        queue.Enqueue((start.row, start.col, 0));
+
+        var seen = new HashSet<(int row, int col, int steps)>();
+
+        var destinations = new HashSet<(int row, int col)>();
+
+        while (queue.Any())
+        {
+            // Console.WriteLine($"Queue: {queue.Count}, Seen: {seen.Count}, Destinations: {destinations.Count}");
+
+            var current = queue.Dequeue();
+
+            if (seen.Contains(current))
+            {
+                continue;
+            }
+
+            seen.Add(current);
+
+            var (row, col, steps) = current;
+            var position = (row, col);
+
+            if (current.steps > goal)
+            {
+                throw new Exception($"Too many steps ({current.steps})");
+            }
+
+            if (current.steps == goal)
+            {
+                // if (destinations.Contains((row, col)))
+                // {
+                //     throw new Exception($"Already visited ({row},{col})");
+                // }
+
+                destinations.Add((row, col));
+                continue;
+            }
+
+            var neighbors = new (int row, int col)[]
+            {
+                (row - 1, col),
+                (row + 1, col),
+                (row, col - 1),
+                (row, col + 1),
+            };
+
+            foreach (var neighbor in neighbors)
+            {
+                if (grid.Contains(neighbor))
+                {
+                    queue.Enqueue((neighbor.row, neighbor.col, steps + 1));
+                }
+            }
+        }
+
+        return destinations.Count;
     }
 
-    public static int Solve2(string input)
+    private static (HashSet<(int row, int col)> grid, (int row, int col) start) ParseInput(string[] input)
+    {
+        var grid = new HashSet<(int row, int col)>();
+        var start = (0, 0);
+
+        for (var row = 0; row < input.Length; row++)
+        {
+            for (var col = 0; col < input[row].Length; col++)
+            {
+                if (input[row][col] is 'S')
+                {
+                    start = (row, col);
+                }
+
+                if (input[row][col] is '.' or 'S')
+                {
+                    grid.Add((row, col));
+                }
+            }
+        }
+
+        return (grid, start);
+    }
+
+    private static string DrawGrid(HashSet<(int row, int col)> grid, (int, int) start)
+    {
+        var sb = new StringBuilder();
+
+        var minRow = grid.Min(x => x.row);
+        var maxRow = grid.Max(x => x.row);
+        var minCol = grid.Min(x => x.col);
+        var maxCol = grid.Max(x => x.col);
+
+        for (var row = minRow; row <= maxRow; row++)
+        {
+            for (var col = minCol; col <= maxCol; col++)
+            {
+                if ((row, col) == start)
+                {
+                    sb.Append('S');
+                }
+                else
+                {
+                    sb.Append(grid.Contains((row, col)) ? '.' : '#');
+                }
+            }
+
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+
+    public static int Solve2(string[] input)
     {
         return 0;
     }
--- tests/aoc-2023-csharp-tests/Day21Tests.cs
+++ tests/aoc-2023-csharp-tests/Day21Tests.cs
@@ -4,15 +4,24 @@ namespace aoc_2023_csharp_tests;
 
 public class Day21Tests
 {
-    [Test]
-    public void Part1_Example()
+    [TestCase(new[]
     {
-        // arrange
-        var input = "";
-        var expected = 0;
-
+        "...........",
+        ".....###.#.",
+        ".###.##..#.",
+        "..#.#...#..",
+        "....#.#....",
+        ".##..S####.",
+        ".##..#...#.",
+        ".......##..",
+        ".##.#.####.",
+        ".##..##.##.",
+        "...........",
+    }, 6, 16)]
+    public void Part1_Example(string[] input, int steps, int expected)
+    {
         // act
-        var actual = Day21.Solve1(input);
+        var actual = Day21.Solve1(input, steps);
 
         // assert
         actual.Should().Be(expected);
@@ -21,14 +30,18 @@ public class Day21Tests
     [Test]
     public void Part1_Solution()
     {
-        Day21.Part1().Should().Be(0);
+        Day21.Part1().Should().Be(3689);
     }
 
     [Test]
     public void Part2_Example()
     {
         // arrange
-        var input = "";
+        var input = new[]
+        {
+            "",
+        };
+
         var expected = 0;
 
         // act
